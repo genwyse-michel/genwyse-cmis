a9168276cfa6922a5505dc447b2dd2652474edef
mreverbel <michel.reverbel@genwyse.com>
2019-11-06 15:08:16 +0000

    Adaptation du script pour installer d'autres versions
    git-svn-id: http://svn.genwyse.com/svn/Genwyse/Java/Genwyse%20CMIS/trunk@3160 1a4762fb-089e-4947-8fb2-3235008ee9d0

--- dist/install.sh
+++ dist/install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+version=$1
+file=genwyse-cmis
+if [ "$version" == "" ]; then
+  version=`/bin/ls $file-*.jar| sed "s/^$file-//"|sed 's/\.jar$//'|tail -1`
+fi
+echo "Installation de la version $version"
+
 mvn install:install-file \
   -DgroupId=com.genwyse.cmis \
   -DartifactId=genwyse-cmis \
   -Dpackaging=jar \
-  -Dversion=1.1.0 \
-  -Dfile=genwyse-cmis-1.1.0.jar \
+  -Dversion=$version \
+  -Dfile=genwyse-cmis-$version.jar \
   -DgeneratePom=true
